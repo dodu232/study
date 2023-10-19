6b91754c49a1180b2c200fa8cbef5adeb0cfc364
dodu232 <114296489+dodu232@users.noreply.github.com>
2023-10-20 01:26:53 +0900

    231020 helloworld

---
+++ Hello/MainApp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using static System.Console;
+
+namespace Hello
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            if(args.Length == 0)
+            {
+                Console.WriteLine("사용법: Hello.exe <서보경>");
+                return;
+            }
+            WriteLine("Hello, {0}!", args[0]);
+        }
+    }
+}
